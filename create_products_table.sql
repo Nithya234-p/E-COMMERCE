50b44292763c86f810fef24a4aecdacbf3da6bee
Nithya234-p <nithyapvnk@gmail.com>
2025-07-31 10:57:43 +0530

    Update create_products_table.sql

--- create_products_table.sql
+++ create_products_table.sql
@@ -1,2 +1,13 @@
 git init
 git add create_products_table.sql
+CREATE TABLE Products (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    cost FLOAT,
+    category NVARCHAR(100),
+    name NVARCHAR(255),
+    brand NVARCHAR(100),
+    retail_price FLOAT,
+    department NVARCHAR(100),
+    sku NVARCHAR(100),
+    distribution_centre_id NVARCHAR(100)
+);
