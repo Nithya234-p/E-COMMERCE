01a3f9e994ac0ca8a10ea84fc540103d0849e7d7
Nithya234-p <nithyapvnk@gmail.com>
2025-07-31 12:05:32 +0530

    Update create_products_table.sql

--- create_products_table.sql
+++ create_products_table.sql
@@ -9,4 +9,12 @@ CREATE TABLE Products (
     sku NVARCHAR(100),
     distribution_centre_id NVARCHAR(100)
 );
-SELECT TOP 10 * FROM Products
+
+INSERT INTO Products (
+    cost, name, brand, retail_price, department, sku, category, distribution_centre_id
+)
+VALUES
+(400.00, 'Product 1', 'Brand A', 499.99, 'Electronics', 'SKU001', 'Phones', 'DC001'),
+(600.00, 'Product 2', 'Brand B', 799.00, 'Home appliances', 'SKU002', 'Kitchen', 'DC002');
+
+SELECT TOP 10 * FROM Products;
